8b9f92a6c2835f9d1232ac035f887048847ed873
AidanSaull <saull.aidan@gmail.com>
2022-12-04 00:56:29 -0500

    CODE player scores now start at 0

--- src/main/java/com/aidansaull/crazyEights/Player.java
+++ src/main/java/com/aidansaull/crazyEights/Player.java
@@ -7,9 +7,11 @@ import java.util.List;
 public class Player
 {
     public List<Card> hand;
+    public Integer score;
 
     public Player()
     {
         hand = new ArrayList<Card>();
+        score = 0;
     }
 }
